ef1acc1be6a11a86ce531f63ef099465740bcb3b
daviddpg <daviddelprado94@gmail.com>
2021-12-04 12:34:48 +0100

    Solve Day01 - Part 1
    It occurred to me that kotlin has the transform lambda in `windowed`
    that could check whether we descended or ascended already by
    substracting. After that it's just a matter of counting the cases where
    we ascended

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,15 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<Int>): Int {
+        return input
+            .windowed(size = 2, transform = { (a, b) -> a - b })
+            .count { it < 0 }
+    }
+
+    fun part2(input: List<Int>): Int {
         return input.size
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInputAsInt("Day01")
     println(part1(input))
     println(part2(input))
 }
